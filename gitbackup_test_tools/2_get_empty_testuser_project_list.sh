aa058dbb35325c1ba15bfbf58218776d5daa11e1
David Rotermund <davrot@uni-bremen.de>
2026-01-03 18:34:12 +0100

    Adapting to the VM B

--- gitbackup_test_tools/2_get_empty_testuser_project_list.sh
+++ gitbackup_test_tools/2_get_empty_testuser_project_list.sh
@@ -4,4 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/config.sh"
 
 \rm -rf ./projects
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${GITBACKUP_SSH_PORT} -i ${DOWNLOADS_DIR}/joe@example.com/.ssh/overleafcep" git clone ssh://joe@example.com@localhost/projects.git
+\rm -f ./overleafcep
+sudo cp ${DOWNLOADS_DIR}/joe@example.com/.ssh/overleafcep .
+sudo chown ubuntu ./overleafcep
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${GITBACKUP_SSH_PORT} -i ./overleafcep" git clone ssh://joe@example.com@localhost/projects.git
+
